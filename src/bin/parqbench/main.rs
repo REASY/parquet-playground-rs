c93cdf4685f207b7815afa9224d4873ac1a99f9d
Artavazd Balaian <reasyu@gmail.com>
2025-01-25 22:29:42 +0800

    feat: Extract number of iterations and whether to use data during the benchmark

--- src/bin/parqbench/main.rs
+++ src/bin/parqbench/main.rs
@@ -9,13 +9,15 @@ static GLOBAL: Jemalloc = Jemalloc;
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use arrow::array::Array;
-use arrow::datatypes::DataType;
+use arrow::array::{
+    Array, ArrowPrimitiveType, AsArray, GenericStringArray, ListArray, PrimitiveArray,
+};
+use arrow::datatypes::Int64Type;
+use arrow::datatypes::{ArrowNativeType, DataType, Float64Type};
 use clap::{Parser, ValueEnum};
 use parquet::column::reader::get_typed_column_reader;
-use parquet::data_type::{ByteArrayType, DoubleType, Int64Type};
 use parquet::file::reader::{FileReader, SerializedFileReader};
-use parquet::record::Row;
+use parquet::record::{Field, Row};
 use parquet_playground_rs::errors::AppError;
 use parquet_playground_rs::schema::parquet_metadata_to_arrow_schema;
 use parquet_playground_rs::vec_pq_reader::{read_f64_column, read_i64_column, read_string_column};
@@ -41,6 +43,136 @@ struct AppArgs {
     /// The type of Parquet reader
     #[clap(long)]
     parquet_reader_type: ParquetReaderType,
+    /// Flag to control whether it should use read data from Parquet
+    #[clap(long)]
+    use_data: bool,
+    /// Number of iterations to run the benchmark
+    #[clap(long, default_value_t = 100)]
+    iterations: usize,
+}
+
+fn touch_primitive_array<T: ArrowPrimitiveType>(arr: &PrimitiveArray<T>) -> usize {
+    let mut total: usize = 0;
+    for i in 0..arr.len() {
+        if arr.is_null(i) {
+            total += 1;
+        } else {
+            let v = arr.value(i);
+            total += v.as_usize();
+        }
+    }
+    total
+}
+
+fn touch_string_array(arr: &GenericStringArray<i32>) -> usize {
+    let mut total: usize = 0;
+    for i in 0..arr.len() {
+        if arr.is_null(i) {
+            total += 1;
+        } else {
+            let v = arr.value(i);
+            total += v.len();
+        }
+    }
+    total
+}
+
+fn touch_list_array(arr: &ListArray) -> usize {
+    let mut total: usize = 0;
+    for i in 0..arr.len() {
+        if arr.is_null(i) {
+            total += 1;
+        } else {
+            total += touch_array(&arr.value(i));
+        }
+    }
+    total
+}
+
+fn touch_array(array: &dyn Array) -> usize {
+    let mut total: usize = 0;
+
+    match array.data_type() {
+        DataType::Null => {
+            total += 1;
+        }
+        DataType::Int64 => {
+            let arr: &PrimitiveArray<Int64Type> = array.as_primitive();
+            total += touch_primitive_array(arr);
+        }
+        DataType::Float64 => {
+            let arr: &PrimitiveArray<Float64Type> = array.as_primitive();
+            total += touch_primitive_array(arr);
+        }
+        DataType::Utf8 => {
+            let arr: &GenericStringArray<i32> = array.as_string();
+            total += touch_string_array(arr);
+        }
+        DataType::List(list) => {
+            list.as_ref();
+            match list.data_type() {
+                DataType::Null => {
+                    total += 1;
+                }
+                DataType::Int64 => {
+                    let arr = array.as_any().downcast_ref::<ListArray>().unwrap();
+                    total += touch_list_array(arr);
+                }
+                DataType::Float64 => {
+                    let arr = array.as_any().downcast_ref::<ListArray>().unwrap();
+                    total += touch_list_array(arr);
+                }
+                DataType::Utf8 => {
+                    let arr = array.as_any().downcast_ref::<ListArray>().unwrap();
+                    total += touch_list_array(arr);
+                }
+                x => panic!("Unexpected data type of List {:?}", x),
+            }
+        }
+        x => panic!("Unexpected data type {:?}", x),
+    }
+    total
+}
+
+fn touch_row(row: &Row) -> usize {
+    let mut total: usize = 0;
+    for (_, field) in row.get_column_iter() {
+        match field {
+            Field::Null => {
+                total += 1;
+            }
+            Field::Long(v) => {
+                total += *v as usize;
+            }
+            Field::Double(v) => {
+                total += *v as usize;
+            }
+            Field::Str(str) => {
+                total += str.len();
+            }
+            Field::ListInternal(list) => {
+                for e in list.elements() {
+                    match e {
+                        Field::Null => {
+                            total += 1;
+                        }
+                        Field::Long(v) => {
+                            total += *v as usize;
+                        }
+                        Field::Double(v) => {
+                            total += *v as usize;
+                        }
+                        Field::Str(str) => {
+                            total += str.len();
+                        }
+                        x => panic!("Unexpected data type of ListInternal {:?}", x),
+                    }
+                }
+            }
+            x => panic!("Unexpected data type {:?}", x),
+        }
+    }
+    total
 }
 
 fn main() -> errors::Result<()> {
@@ -48,29 +180,40 @@ fn main() -> errors::Result<()> {
 
     let args = AppArgs::parse();
     info!("Received args: {:?}", args);
-    const ITERS: usize = 100;
 
     let t: Instant = Instant::now();
-    let mut total: usize = 0;
-    for _i in 0..ITERS {
+    let mut counter: usize = 0;
+    for _ in 0..args.iterations {
         match args.parquet_reader_type {
             ParquetReaderType::RowByRow => {
                 let xs = read_parquet(args.input_parquet_file_path.as_str())?;
-                total += xs.len();
+                if args.use_data {
+                    for row in &xs {
+                        counter += touch_row(row);
+                    }
+                } else {
+                    counter += xs.len();
+                }
             }
             ParquetReaderType::Columnar => {
                 let arrays = read_parquet_v2(args.input_parquet_file_path.as_str())?;
-                // The first column is not a List, so we use it to calculate the total number of rows
-                arrays.first().iter().for_each(|arr| {
-                    total += arr.len();
-                });
+                if args.use_data {
+                    for array in &arrays {
+                        counter += touch_array(array.as_ref());
+                    }
+                } else {
+                    // The first column is not a List, so we use it to calculate the total number of rows
+                    arrays.first().iter().for_each(|arr| {
+                        counter += arr.len();
+                    });
+                }
             }
         };
     }
     let total_elapsed_ms = t.elapsed().as_millis() as f64;
-    let avg = total_elapsed_ms / ITERS as f64;
+    let avg = total_elapsed_ms / args.iterations as f64;
     info!(
-        "Total time is {total_elapsed_ms:.3} ms, average reading time is {avg:.3} ms, the number of rows in parquet is {}", total / ITERS
+        "Total time is {total_elapsed_ms:.3} ms, average reading time is {avg:.3} ms. Counter is {}", counter
     );
 
     Ok(())
@@ -102,28 +245,29 @@ fn read_parquet_v2(path: &str) -> Result<Vec<Box<dyn Array>>, AppError> {
         let col_desc = rg.metadata().column(col_idx).column_descr();
         match c.data_type() {
             DataType::Int64 => {
-                let col_rdr = get_typed_column_reader::<Int64Type>(col_rdr);
+                let col_rdr = get_typed_column_reader::<parquet::data_type::Int64Type>(col_rdr);
                 let arr = read_i64_column(col_rdr, col_desc, BATCH_SIZE)?;
                 result.push(arr);
             }
             DataType::Float64 => {
-                let col_rdr = get_typed_column_reader::<DoubleType>(col_rdr);
+                let col_rdr = get_typed_column_reader::<parquet::data_type::DoubleType>(col_rdr);
                 let arr = read_f64_column(col_rdr, col_desc, BATCH_SIZE)?;
                 result.push(arr);
             }
             DataType::Utf8 | DataType::Binary => {
-                let col_rdr = get_typed_column_reader::<ByteArrayType>(col_rdr);
+                let col_rdr = get_typed_column_reader::<parquet::data_type::ByteArrayType>(col_rdr);
                 let arr = read_string_column(col_rdr, col_desc, BATCH_SIZE)?;
                 result.push(arr);
             }
             DataType::List(field) => match field.data_type() {
                 DataType::Int64 => {
-                    let col_rdr = get_typed_column_reader::<Int64Type>(col_rdr);
+                    let col_rdr = get_typed_column_reader::<parquet::data_type::Int64Type>(col_rdr);
                     let arr = read_i64_column(col_rdr, col_desc, BATCH_SIZE)?;
                     result.push(arr);
                 }
                 DataType::Float64 => {
-                    let col_rdr = get_typed_column_reader::<DoubleType>(col_rdr);
+                    let col_rdr =
+                        get_typed_column_reader::<parquet::data_type::DoubleType>(col_rdr);
                     let arr = read_f64_column(col_rdr, col_desc, BATCH_SIZE)?;
                     result.push(arr);
                 }
